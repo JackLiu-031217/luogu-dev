c0d1943e7e7e20a25218cc3e5fa502fec9aa310c
Just-Prog <JustProg5835@outlook.com>
2021-02-19 09:18:29 +0800

    重写P1003逻辑

--- 洛谷P1003.cpp
+++ 洛谷P1003.cpp
@@ -8,24 +8,26 @@
 //现在将这些地毯按照编号从小到大的顺序平行于坐标轴先后铺设，后铺的地毯覆盖在前面已经铺好的地毯之上。
 //地毯铺设完成后，组织者想知道覆盖地面某个点的最上面的那张地毯的编号。注意：在矩形地毯边界和四个顶点上的点也算被地毯覆盖。
 //https://www.luogu.com.cn/problem/P1003
+//部分代码借鉴了https://clear-skies.blog.luogu.org/solution-p1003
 
 #include<iostream>
 using namespace std;
-int j[100001][100001]={0}; //地板坐标
 int main(){
     int n; //地毯数，1<=n<=10000
+    cin>>n;
+    int a[10001],b[10001],g[10001],k[10001];
     for(int i=1;i<=n;i++){
-        int a,b,g,k;
-        cin>>a>>b>>g>>k; //注意:(a,b)是地毯左下角的坐标.g,k是该块地毯x,y轴上的长度
-        for(int l=b;l>=b-k;l--){ //b需要逆推
-            for(int m=a;m<=a+g;m++){ //a需要顺推
-                j[m][l]=i;
-            }
-        }
+        cin>>a[i]>>b[i]>>g[i]>>k[i]; //注意:(a,b)是地毯左下角的坐标.g,k是该块地毯x,y轴上的长度
     }
     int x,y; //判断用的点
-    if(j[x][y]<1){
-        cout<<"-1"<<endl;
-    }else cout<<j[x][y]<<endl;
+    bool l=0;
+    cin>>x>>y;
+    for(int d=n;d>=1;d--){ //逆序倒推
+        if(x>=a[d]&&x<=a[d]+g[d]&&y<=b[d]+k[d]&&y>=b[d]){
+            cout<<x<<" "<<y<<endl;
+            return 0;
+        }
+    }
+    cout<<"-1"<<endl;
     return 0;
 }
