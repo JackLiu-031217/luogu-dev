25b7ef2850d66c8d4ca2d65b46bd61869ea50114
Just-Prog <JustProg5835@outlook.com>
2021-02-24 08:21:06 +0800

    逻辑重构 由 https://www.luogu.com.cn/blog/user262/solution-p1031 提供思路

--- 洛谷P1031.cpp
+++ 洛谷P1031.cpp
@@ -2,41 +2,38 @@
     P1031 [NOIP2002 提高组] 均分纸牌
         NOIP 2002 提高组第一题
 --------------------------------------*/
+//https://www.luogu.com.cn/problem/P1031
+//https://www.luogu.com.cn/record/47010311
 #include<iostream>
 using namespace std;
 int main(){
     int n; //纸牌堆数
-    int p[101]; //每一堆纸牌包含的纸牌数目
+    cin>>n;
+    int p[n]; //每一堆纸牌包含的纸牌数目
     int z=0; //纸牌总数
+    int ans=0; //移动次数
     for(int i=1;i<=n;i++){
         cin>>p[i];
         z+=p[i];
     }
-    int pj=z/n;
+    z/=n;
+
+    //以下部分主要架构来自于 https://www.luogu.com.cn/blog/user262/solution-p1031
+    //感谢前辈 谁懂谁伤心 于 2013-11-22 发布的题解
+    //前辈的智慧不可小视
+    //本人的程序性思维还有待提升
+    
     for(int i=1;i<=n;i++){
-        if(i==1){
-            if(p[1]<pj){
-                p[2]-=pj-p[1];
-                p[1]=pj;
-            }else if(p[1]>pj){
-                p[2]+=p[1]-pj;
-                p[1]=pj;
-            }else if(p[1]==pj){
-                continue;
-            }
-        }
-        if(i==n){
-            if(p[n]>pj){
-                p[n-1]+=p[n]-pj;
-                p[n]=pj;
-                i=1;
-            }else if(p[n]<pj){
-                p[n-1]-=pj-p[n];
-                p[n]=pj;
-                i=1;
-            }else if(p[n]==pj){
-                i=1;
-            }
-        }
+        p[i]-=z;
     }
+
+    for(int i=1;i<=n;i++){
+        if(p[i]==0){
+            continue;
+        }
+        p[n+1]+=p[i];
+        ans++;
+    }
+    cout<<ans;
+    return 0;
 }
