6b8dc6c97587ffbcdfc733e6f2de1ad3f4cf9139
Just-Prog <JustProg5835@outlook.com>
2021-07-19 15:11:23 +0800

    P5728 旗鼓相当的对手

--- 洛谷P5728.cpp
+++ 洛谷P5728.cpp
@@ -1,22 +1,46 @@
-//第一次提交:https://www.luogu.com.cn/record/34785861 20分
+/*---------------------------------------------
+        P5728 【深基5.例5】旗鼓相当的对手       
+---------------------------------------------*/
+//https://www.luogu.com.cn/problem/P5728
+
+/*
+  现有 N(N<=1000) 名同学参加了期末考试，并且获得了每名同学的信息：
+  语文、数学、英语成绩（均为不超过 150 的自然数）。
+  如果某对学生 <i,j> 的每一科成绩的分差都不大于 5，且总分分差不大于 10，那么这对学生就是“旗鼓相当的对手”。
+  现在想知道这些同学中，有几对“旗鼓相当的对手”？同样一个人可能会和其他好几名同学结对。
+*/
+
+//https://www.luogu.com.cn/record/34785861
+//https://www.luogu.com.cn/record/53184594
+//https://www.luogu.com.cn/record/53571412
+
 #include<iostream>
 #include<cmath>
 using namespace std;
 int main(){
-    int n,student[1001][4],sum[1001],qiguxiangdang=0;//善用二维数组
+    int n,stu[1001][3],stuall[1001]={0},qgxd=0;
     cin>>n;
-    for(int i=1;i<=n;i++){
-        cin>>student[i][1]>>student[i][2]>>student[i][3];
-        sum[i]=student[i][1]+student[i][2]+student[i][3];
+    for(int i=0;i<n;i++){
+        for(int o=0;o<=2;o++){
+            cin>>stu[i][o];
+            stuall[i]+=stu[i][o];
+        }
     }
-    for(int i=1;i<=n;i++){
-        for(int j=i+1;j<=n;j++){
-            //abs:绝对值求值函数，头文件为cmath或math.h
-            if(abs(student[i][1]-student[j][1])<=5 && abs(student[i][2]-student[j][2])<=5 && abs(student[i][3]-student[j][3])<=5 && sum[i]-sum[j]<=10){
-                qiguxiangdang++;
+    for(int i=0;i<n-1;i++){
+        for(int o=i+1;o<n;o++){
+            int qgflag=0;
+            for(int k=0;k<=2;k++){
+                if(abs(stu[i][k]-stu[o][k])<=5){
+                    qgflag++;
+                }
+            }
+            if(qgflag==3){
+                if(abs(stuall[i]-stuall[o])<=10){
+                    qgxd++;
+                }
             }
         }
     }
-    cout<<qiguxiangdang<<endl;
+    cout<<qgxd<<endl;
     return 0;
 }
