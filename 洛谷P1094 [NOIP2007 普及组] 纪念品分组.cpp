40a8b9dd7478203c975050d1001d46ec7df09b42
Just-Prog <JustProg5835@outlook.com>
2021-11-04 17:16:15 +0800

    add description for 1094

--- 洛谷P1094 [NOIP2007 普及组] 纪念品分组.cpp
+++ 洛谷P1094 [NOIP2007 普及组] 纪念品分组.cpp
@@ -1,3 +1,6 @@
+//P1094 [NOIP2007 普及组] 纪念品分组
+//https://www.luogu.com.cn/problem/P1094
+
 #include<iostream>
 #include<algorithm>
 using namespace std;
@@ -8,6 +11,8 @@ int main(){
         cin>>p[i];
     }
     sort(p+1,p+1+g);
+
+    //https://www.luogu.com.cn/blog/_post/2968
     int h=1,e=g,ans=0;
     while(h<=e){
         if(p[h]+p[e]<=w){
