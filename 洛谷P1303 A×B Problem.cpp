eed4cb0ff71261f20972fd775291bfb5cd0c36fc
Just-Prog <JustProg5835@outlook.com>
2022-05-26 17:19:50 +0800

    P1303 A*B Problem *模拟,字符串,高精度

---
+++ 洛谷P1303 A×B Problem.cpp
@@ -0,0 +1,63 @@
+// Problem: P1303 A*B Problem
+// Contest: Luogu
+// URL: https://www.luogu.com.cn/problem/P1303
+// Memory Limit: 125 MB
+// Time Limit: 1000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+
+//https://www.luogu.com.cn/record/39282081  20 不详
+//https://www.luogu.com.cn/record/76417190  0  没整 0 的特判
+//以下 0 分原因详见下文
+//https://www.luogu.com.cn/record/76417270  0
+//https://www.luogu.com.cn/record/76417420  0
+//https://www.luogu.com.cn/record/76417492  0
+//https://www.luogu.com.cn/record/76417678  0
+//https://www.luogu.com.cn/record/76417817  100
+
+#include<iostream>
+using namespace std;
+int a[20000],b[20000],c[40000];
+int main(){
+    string aa,bb;
+    cin>>aa;
+    cin>>bb;
+
+    //以下内容对于 cpp14 / gcc9 没用
+    //同时导致了 5 次查错的失败
+    //getline(cin,aa);
+    //getline(cin,bb);
+
+    if(aa=="0"||bb=="0"){
+        cout<<0<<endl;
+        return 0;
+    }
+    int aaa=aa.length(),bbb=bb.length();
+    for(int i=aaa-1;i>=0;i--){
+        a[aaa-i]=aa[i]-'0';
+    }
+    for(int i=bbb-1;i>=0;i--){
+        b[bbb-i]=bb[i]-'0';
+    }
+    for(int i=1;i<=aaa;i++){
+        for(int o=1;o<=bbb;o++){
+            c[i+o-1]+=a[i]*b[o]; //乘法竖式模拟
+        }
+    }
+    for(int i=1;i<=aaa+bbb;i++){
+        if(c[i]>=10){
+            c[i+1]+=c[i]/10;
+            c[i]%=10;
+        }
+    }
+    int d=aaa+bbb;
+    while(c[d]==0&&d>=1){
+        d--;
+        cerr<<d<<endl;
+    }
+    for(int i=d;i>=1;i--){
+        cout<<c[i];
+    }
+    cout<<endl;
+    return 0;
+}
