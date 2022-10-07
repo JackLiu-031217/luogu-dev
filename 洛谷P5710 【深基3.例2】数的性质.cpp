5bcc8d11304a98cd73ac6776850d6da46b005c41
Just-Prog <JustProg5835@outlook.com>
2022-10-07 18:53:32 +0800

    P5710 - 更新做法 * 原做法似乎是抄自 https://crp-qwq-16-8.blog.luogu.org/solution-p5710 的 * 涉及到了完全没有掌握的异或位运算符知识点 * 特此重置

--- 洛谷P5710 【深基3.例2】数的性质.cpp
+++ 洛谷P5710 【深基3.例2】数的性质.cpp
@@ -1,29 +1,11 @@
 //P5710 【深基3.例2】数的性质
 //深入浅出
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
 int main(){
     int i;
     cin>>i;
-    if(i%2==0&&(i>4&&i<=12)){
-        cout<<"1 ";
-    }else{
-        cout<<"0 ";
-    }
-    if(i%2==0||(i>4&&1<=12)){
-        cout<<"1 ";
-    }else{
-        cout<<"0 ";
-    }
-    if(i%2==0^(i>4&&1<=12)){
-        cout<<"1 ";
-    }else{
-        cout<<"0 ";
-    }
-    if(i%2!=0&&(i<4||1>12)){
-        cout<<"1";
-    }else{
-        cout<<"0";
-    }
+    bool a=!(i%2),b=(i>4&&i<=12);
+    printf("%d %d %d %d\n",a&&b,a||b,((a&&!b)||(!a&&b)),!a&&!b);
     return 0;
 }
